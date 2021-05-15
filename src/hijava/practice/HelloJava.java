00ccb7d0db74cab2a598c612d5d061b0c1ce70fc
a <a@192.168.219.112>
2021-05-15 11:30:58 +0900

    첫번째 커밋

---
+++ src/hijava/practice/HelloJava.java
@@ -0,0 +1,24 @@
+package hijava.practice;
+
+/**
+ * 첫번재 자바 실습
+ * @author Java
+ *
+ */
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		HelloJava.say("Hi~");
+		
+		// JVM이 스스로 화면에 출력
+		System.out.println("Hello Java!!");
+		 
+		
+	}
+
+	public static void say(String msg) {
+		System.out.println(msg);
+	}
+}
